e65cba671e6c7d213172330824ea52d4ec7a190e
Aurko1083 <mahbubaurko23@gmail.com>
2024-11-30 12:09:47 +0600

    dark trhemee

--- src/components/theme-loader.tsx
+++ src/components/theme-loader.tsx
@@ -1,27 +1,15 @@
-import { themeAtom } from "@/lib/store"
-import { useAtomValue } from "jotai"
-import { useEffect } from "react"
+import { useEffect } from "react";
 
 export function ThemeLoader() {
-	const theme = useAtomValue(themeAtom)
+    useEffect(() => {
+        const root = window.document.documentElement;
 
-	useEffect(() => {
-		const root = window.document.documentElement
+       
+        root.classList.remove("light");
+        
+        
+        root.classList.add("dark");
+    }, []);
 
-		root.classList.remove("light", "dark")
-
-		if (theme === "system") {
-			const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
-				.matches
-				? "dark"
-				: "light"
-
-			root.classList.add(systemTheme)
-			return
-		}
-
-		root.classList.add(theme)
-	}, [theme])
-
-	return null
+    return null;
 }
